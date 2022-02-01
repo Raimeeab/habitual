652d3591aa8e8b1a2c563d121a61adae88153bcb
Raimeeab <raimee.abbassi@gmail.com>
2022-02-01 12:22:25 +1100

    Set up front-end auth

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,11 +1,37 @@
 import decode from "jwt-decode";
 
 class AuthService {
-  getUser() {
+  getProfile() {
     return decode(this.getToken());
-  };
+  }
 
+  loggedIn() {
+    const token = this.getToken();
+    !!token && !this.isTokenExpired(token);
+  }
 
-};
+  isTokenExpired(token) {
+    const decoded = decode(token);
+    if (decoded.exp < Date.now()) {
+      localStorage.removeItem("id_token");
+      return true;
+    }
+    return false;
+  }
 
-export default new AuthService(); 
+  getToken() {
+    return localStorage.getItem("id_token");
+  }
+
+  login(idToken) { 
+    localStorage.setItem("id_token", idToken); 
+    window.location.assign("/"); 
+  }
+
+  logout() { 
+    localStorage.removeItem("id_token"); 
+    window.location.reload(); 
+  }
+}
+
+export default new AuthService();
