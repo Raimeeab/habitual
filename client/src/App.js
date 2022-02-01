d351a4cc7c5cc6a1a80ea42fade54ae2ecd12f1e
Raimeeab <raimee.abbassi@gmail.com>
2022-02-01 12:44:34 +1100

    Clean install & set up Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Holds global styles 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+// Holds global styles
 import "./App.css";
 
 // App components
@@ -9,21 +16,45 @@ import Home from "./components/Home/Home";
 import Habits from "./components/Habits/Habits";
 import AccountBox from "./components/AccountBox";
 
+// Construct main GraphQl API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Contrust request middleware 
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem("id_token"); 
+  
+  // Will attach the JWT Token to every request as "authorization" header 
+  return { 
+    headers: { 
+      ...headers, 
+      authorization: token ? `Bearer ${token}` : '', 
+    } 
+  }; 
+}); 
+
+// Set up client to execute authLink middleware prior to GQL API request 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), 
+  cache: new InMemoryCache(), 
+}); 
+
 function App() {
   return (
     // TODO: Before deployment add
     //       basename="Habitual" in <Router>
-    <>
+    <ApolloProvider client={client}>
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/login" element={<Login />} /> */}
           <Route path="/habits" element={<Habits />} />
-          <Route path="/login" element={<AccountBox/>} />
+          <Route path="/login" element={<AccountBox />} />
         </Routes>
       </Router>
-    </>
+    </ApolloProvider >
   );
 }
 
